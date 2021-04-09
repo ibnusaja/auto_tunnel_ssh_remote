4015abf01d89a9e480d208efbb48ac47cb17c586
ibnusaja <45093115+ibnusaja@users.noreply.github.com>
2021-04-09 09:04:23 +0800

    Update tanel.sh

--- tanel.sh
+++ tanel.sh
@@ -1,2 +1,2 @@
 ssh -NR portvps:localhost:portlocal user-vps@ip-vps;
-/data/data/com.termux/files/home/auto_tunnel_ssh_remot/file2.sh
+/data/data/com.termux/files/home/auto_tunnel_ssh_remote/file2.sh
